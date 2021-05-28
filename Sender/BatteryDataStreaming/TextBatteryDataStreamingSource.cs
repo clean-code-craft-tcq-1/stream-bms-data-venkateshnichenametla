b0fd93ac2ba1d781d90a0f23e3152e52570e2024
venkateshnichenametla <77480058+venkateshnichenametla@users.noreply.github.com>
2021-05-28 14:09:12 +0530

    Add files via upload

---
+++ Sender/BatteryDataStreaming/TextBatteryDataStreamingSource.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+namespace BatteryDataStreaming
+{
+    public class TextBatteryDataStreamingSource : IBatteryDataStreamingSource
+    {
+        private string[] batteryData;
+        public string GetBatteryParameterBasedOnIndex(int index)
+        {
+            if(batteryData != null && batteryData.Length > 0)
+                return batteryData[index];
+            return string.Empty;
+        }
+
+        public void LoadBatteryParameters(string filepath)
+        {
+            if (!File.Exists(filepath))
+                throw new FileNotFoundException();
+            batteryData  = File.ReadAllLines(filepath);
+        }
+    }
+}
