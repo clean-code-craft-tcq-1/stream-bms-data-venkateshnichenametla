14e33e504264ddd29794773d654c3c60d589049b
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-06-21 15:08:05 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,5 @@
 pushd "%~dp0"
 
-dotnet run --project Sender\BatteryDataStreaming\BatteryDataStreaming.csproj | dotnet run --project Receiver\BatteryStreamReceiver\BatteryStreamReceiver.csproj
-
-cmd /c exit -1073741510
+timeout 1 dotnet run --project Sender\BatteryDataStreaming\BatteryDataStreaming.csproj | dotnet run --project Receiver\BatteryStreamReceiver\BatteryStreamReceiver.csproj
 
 popd
