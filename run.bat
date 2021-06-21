f2a7d68f9a0fa826204e510a47696d08efc15d50
Vaibhav Bhapkar <35737937+VaibhavBhapkar@users.noreply.github.com>
2021-06-21 14:52:24 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,4 +2,6 @@ pushd "%~dp0"
 
 dotnet run --project Sender\BatteryDataStreaming\BatteryDataStreaming.csproj | dotnet run --project Receiver\BatteryStreamReceiver\BatteryStreamReceiver.csproj
 
+cmd /c exit -1073741510
+
 popd
